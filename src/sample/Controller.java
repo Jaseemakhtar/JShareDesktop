e9c478327d7dc4acd9d2e5e2bf93f7dfbd2e588b
Jaseemakhtar <jaseemamaljeri@gmail.com>
2019-01-14 11:46:17 +0530

    third commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,21 +1,45 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class Controller implements Initializable, EventHandler<ActionEvent> {
     public TextField txtIPAddress;
     public Button btnConnect;
     public Button btnSend;
     public Button btnReceive;
-    public ListView listView;
+    public TableView tableView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        btnConnect.setOnAction(this);
+        btnReceive.setOnAction(this);
+        btnSend.setOnAction(this);
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if(event.getSource() == btnSend)
+            send();
+        else if(event.getSource() == btnConnect)
+            connect();
+        else
+            receive();
+    }
+
+    private void receive() {
+
+    }
+
+    private void connect() {
+
+    }
+
+    private void send() {
 
     }
 }
