31b7be550a3ccaf7ed6ab294fd0aa5d1e4d87140
Jaseemakhtar <jaseemamaljeri@gmail.com>
2019-01-14 11:36:39 +0530

    second commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public TextField txtIPAddress;
+    public Button btnConnect;
+    public Button btnSend;
+    public Button btnReceive;
+    public ListView listView;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
